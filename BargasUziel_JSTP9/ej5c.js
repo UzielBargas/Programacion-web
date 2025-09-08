5d36398124e4f49059ebfc7f59c71650ad02f11c
Uziel Bargas <uzielbargas1@gmail.com>
2025-09-08 18:26:09 -0300

    tp9

---
+++ BargasUziel_JSTP9/ej5c.js
@@ -0,0 +1,27 @@
+var DOLAR = 1000;  
+var REAL = 200;    
+var EURO = 1100;   
+
+function pesosADolares(pesos) {
+    return pesos / DOLAR;
+}
+
+function dolaresAPesos(dolares) {
+    return dolares * DOLAR;
+}
+
+function pesosAReales(pesos) {
+    return pesos / REAL;
+}
+
+function realesAPesos(reales) {
+    return reales * REAL;
+}
+
+function pesosAEuros(pesos) {
+    return pesos / EURO;
+}
+
+function eurosAPesos(euros) {
+    return euros * EURO;
+}
